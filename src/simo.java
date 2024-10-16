c5a5f1fe551acf0ce3cc1e01638c6dac04445dab
chaoura <chaoura93@gmail.com>
2024-10-16 15:37:19 +0100

    add-class

--- src/simo.java
+++ src/simo.java
@@ -1,2 +1,18 @@
 public class simo {
+    private String card;
+    private int score;
+    public simo(String card, int score) {
+        this.card = card;
+        this.score = score;
+
+    }
+    public String getCard() {
+        return card;
+    }
+    public int getScore() {
+        return score;
+
+    }
+
+
 }
